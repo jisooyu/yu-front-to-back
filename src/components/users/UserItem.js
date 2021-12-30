0807af6b72e6dd83149eb1e4c40a891a2142f411
jisooyu <jisooyu@hotmail.com>
2021-12-30 22:34:34 +0900

    first commit

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class UserItem extends Component {
+  render() {
+    const { login, avatar_url, html_url } = this.props.user;
+    return (
+      <div className='card text-center'>
+        <img
+          src={avatar_url}
+          alt=''
+          className='round-img'
+          style={{ width: '60px' }}
+        />
+        <h3>{login}</h3>
+        <div>
+          <a href={html_url}>More </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserItem;

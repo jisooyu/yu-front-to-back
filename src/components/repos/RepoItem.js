45c18ce582ca8bd12718238e08715ec396af7823
jisooyu <jisooyu@hotmail.com>
2022-01-02 16:59:11 +0900

    extrating profile info from users: NOT WORING YET

---
+++ src/components/repos/RepoItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const RepoItem = ({ repo }) => {
+  return <div></div>;
+};
+
+RepoItem.propTypes = {
+  repo: PropTypes.array.isRequired,
+};
+export default RepoItem;
